b36a6445b1a3bcd053ab2c981dc172989372deae
David Laing <david@davidlaing.com>
2010-06-02 07:57:28 +0100

    Added svgdata to map

--- app/models/map.rb
+++ app/models/map.rb
@@ -2,6 +2,7 @@ class Map
   include DataMapper::Resource
   
   property :id,   Serial
-  property :name, String,        :required => true, :length => 500
+  property :name, String,      :required => true, :length => 500
+  property :svgdata, Text,     :required => false 
   timestamps :at 
 end

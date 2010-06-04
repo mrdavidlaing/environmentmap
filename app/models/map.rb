f0003b9a2c988bf3e40ff233e6c9b4e8e87b7187
David Laing <david@davidlaing.com>
2010-06-04 13:19:28 +0100

    Add SVG width and height

--- app/models/map.rb
+++ app/models/map.rb
@@ -3,6 +3,9 @@ class Map
   
   property :id,   Serial
   property :name, String,      :required => true, :length => 500
-  property :svgdata, Text,     :required => false 
+  property :svgdata, Text,     :required => false
+  property :width, Integer,    :required => false
+  property :height, Integer,    :required => false
+
   timestamps :at 
 end
